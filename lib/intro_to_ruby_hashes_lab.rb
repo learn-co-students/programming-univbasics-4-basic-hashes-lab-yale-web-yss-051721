dbaa44722433e494db2c05f0433d4c9c5cc29bf2
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-02 20:16:32 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,23 @@
 def new_hash
-  # return an empty hash
+  Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  a = {
+    :name => "lindsay"
+  }
 end
+p my_hash
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hash = {
+    :name => "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  hash = {
+    :id => number
+  }
 end
